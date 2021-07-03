aaa8fee40720649a68e3921186f3e2b247e81ffe
Labiba Ibnat <labiba.ssp@gmail.com>
2021-07-03 20:17:34 +0600

    add all

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ac68ac6f58f59cf937979954279f0e9d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'df7baf6e-7a12-4cc5-8b87-56f873465a0c'
+};
